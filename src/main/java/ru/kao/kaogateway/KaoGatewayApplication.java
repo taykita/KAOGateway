04b540fb305a0601dde7003915dcb5eb107fd2c7
Artem Korovkin <AOKorovkin@sberbank.ru>
2023-08-04 15:51:36 +0300

    Initial commit

---
+++ src/main/java/ru/kao/kaogateway/KaoGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.kao.kaogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaoGatewayApplication.class, args);
+    }
+
+}
